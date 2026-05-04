b701e8aa7443ebdb190f5eae1d14da0f3879a2a3
yum-higa <yumyuma256@gmail.com>
2026-05-04 23:52:52 +0900

    add: server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log('server started')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('ok');
+});
+
+server.listen(3000, ()=>{
+    console.log('server started');
+});
