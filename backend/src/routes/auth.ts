92624f9e9f515bfaa76f8328f71d63e9f27b1764
yum-higa <yumyuma256@gmail.com>
2026-05-24 22:31:39 +0900

    feat: authの実装

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,16 +1,25 @@
-import { FastifyInstance } from 'fastify';
+import { type FastifyInstance } from 'fastify';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma';
 import { registerSchema, loginSchema } from '../schemas/auth.schema';
+import { REPL_MODE_SLOPPY } from 'node:repl';
+import { loginUser, registerUser } from '../services/auth';
 
 export default async function authRoutes(fastify: FastifyInstance){
-
-    fastify.setErrorHandler((error, request, reply) =>{
-        reply.status(500).send({ ok: false });
+    fastify.post('/register', { schema: registerSchema }, async (request, reply)=>{
+        const { email, password } = request.body as { email: string, password: string }
+        const { token } = await registerUser(email, password);
+        //201はsuccessfully created
+        return reply.status(201).send({ token });
     });
 
-    fastify.post('/register', async (request, reply) => {
-        const { email, password } = request.body as { email}
-    });
+    fastify.post('/login',{ schema:loginSchema }, async (request, reply) =>{
+        const { email, password } = request.body as { email: string, password: string};
+        const { token } = await loginUser( email,password );
+        //statusを省略すると自動的に200になる
+        return reply.send({ token });
+    })
 }
+
+//next action: middleware imp
--- backend/src/services/auth.ts
+++ backend/src/services/auth.ts
@@ -1,11 +1,12 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma';
-import { EmailInUse,MissingJwt } from '../errors';
+import { EmailInUse,InvalidCredentials,MissingJwt } from '../errors';
 
+//this function shouldn't be exposed, thats why no export decralation
 const generateToken = (userId: string): string => {
     const secret = process.env.JWT_SECRET;
-    if (!secret) throw MissingJwt();
+    if (!secret) throw new MissingJwt();
 
     return jwt.sign( { userId }, secret, { expiresIn: '7d'});
 }
@@ -23,13 +24,36 @@ export async function registerUser(email: string, password: string){
 
     const hashed = await bcrypt.hash(password, 10);
 
-    const user = prisma.user.create({
+    const user = await prisma.user.create({
         data: {
             email,
             password: hashed,
         }
     });
 
-    return { id: user.id, email: user.email }
-    //ログイン、レジスターハンドルを思考
+    const token = generateToken(user.id);
+    return { token }
 }
+
+const dummyHashPromise: Promise<string> = bcrypt.hash('__timing_dummy__',10);
+
+export async function loginUser(email: string, password: string) {
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    const hash = user?.password ?? await dummyHashPromise;
+    const isValid = await bcrypt.compare(password, hash);
+
+    if (!isValid || !user) throw new InvalidCredentials;
+
+    const token = generateToken(user.id);
+
+    return { token }
+}
+
+//todo
+// タイミング攻撃の理解
+// bcrypt.compareの理解
